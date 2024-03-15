21608bc7e239bb3d6ceb91ff9768c3ab66677015
rishabhboxpay <rishabh.verma@boxpay.tech>
2024-03-15 19:49:29 +0530

    Updated files

--- router/router.go
+++ router/router.go
@@ -2,9 +2,16 @@ package router
 
 import (
 	"dukan/controllers"
+	"fmt"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+func healthCheck(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,"Health Check Fine")
+}
 
 func Router() *mux.Router{
 	router := mux.NewRouter()
@@ -14,6 +21,7 @@ func Router() *mux.Router{
 	router.HandleFunc("/addProduct",controllers.CreateOneProduct).Methods("POST")		//ok
 	router.HandleFunc("/deleteProduct/{id}",controllers.DeleteOneProduct).Methods("POST")	//ok
 	router.HandleFunc("/updateProduct/{id}",controllers.UpdateOneProduct).Methods("POST")	//ok
+	router.HandleFunc("/health-check", healthCheck).Methods("GET")
 
 	//handle routes to Sales
 	router.HandleFunc("/sales/{date}",controllers.GetAllSales).Methods("GET") 
